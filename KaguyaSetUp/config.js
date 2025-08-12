a3665375985284e74fa57b904aebd9fd0cbbe97c
12mom <74208912+12mom@users.noreply.github.com>
2025-08-12 10:27:09 +0200

    Update config.js

--- KaguyaSetUp/config.js
+++ KaguyaSetUp/config.js
@@ -1,9 +1,10 @@
+// KaguyaSetUp/config.js
 export default {
     "prefix": "*", 
     "BOT_NAME": "ⓜⓘⓚⓞ",
     "ADMIN_IDS": ["61550232547706"],
     "botEnabled": true,
-    "autogreet": true, // تمت إضافة هذه الخاصية
+    "autogreet": true,
     "options": {
         "forceLogin": true,
         "listenEvents": true,
@@ -11,7 +12,7 @@ export default {
         "logLevel": "silent",
         "updatePresence": true,
         "selfListen": true,
-        "usedDatabase":false
+        "usedDatabase": false
     },
     database: {
         type: "json",
@@ -19,6 +20,7 @@ export default {
             uri: "mongodb://0.0.0.0:27017"
         }
     },
-    port: process.env.PORT || 3000,
+    // فقط القيمة الافتراضية، بدون process.env
+    port: 3000,  // سيتم تجاوزه في app.js باستخدام process.env.PORT
     mqtt_refresh: 1200000
 };
